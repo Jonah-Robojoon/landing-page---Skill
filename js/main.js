059d3bc7ecb03fe1cbe07dc66027d499f90c1751
Jonah <38752@ma-web.nl>
2025-01-23 12:27:47 +0100

    final done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 const button = document.getElementById("js--menu");
 const navigation = document.getElementById("js--nav");
 const body = document.getElementById("js--body");
+const hamburger = document.getElementById("hamburger-1")
 
 button.onclick = function() {
+    if (navigation.style.visibility== 'visible'){
+        navigation.style.visibility = "hidden";
+        navigation.style.opacity = 0;
+        body.style.overflow = "visible";
+        hamburger.classList.toggle("is-active");
+    }
+    else{
     navigation.style.visibility = "visible";
     navigation.style.opacity = 1;
     body.style.overflow = "hidden";
+    hamburger.classList.toggle("is-active");
+    }
 }
